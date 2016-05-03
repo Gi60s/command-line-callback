3569223177fa3a904667dd45178d3f960524faa4
James Speirs <james.speirs@gmail.com>
2016-05-03 10:51:36 -0600

    add env example

---
+++ examples/env.js
@@ -0,0 +1,27 @@
+"use strict";
+var Command = require('../index.js');
+var path = require('path');
+
+Command.define('env', callback, {
+    brief: 'This command requires uses an file path',
+    synopsis: [
+        '[OPTIONS]...'
+    ],
+    options: {
+        name: {
+            type: String,
+            description: 'A name to greet you by.',
+            env: 'MY_NAME'
+        },
+        envFile: {              // make a reassignment of the default envFile option to include a default value
+            type: String,
+            description: 'The file path to an environment configuration file that is used to populate environment variables.',
+            defaultValue: path.resolve(__dirname, 'envfile-default.txt')
+        }
+    }
+});
+Command.evaluate();
+
+function callback(config) {
+    console.log('Hello, ' + config.name);
+}
